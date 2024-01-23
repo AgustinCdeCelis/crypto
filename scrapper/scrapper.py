6c0cb673352f420106887e66a80b89149ac4064f
agustin <agustincepeda1@gmail.com>
2024-01-23 17:46:12 -0300

    almost finished

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -11,21 +11,26 @@ import time
 __all__=['Scrapp']
 
 class Scrapp:
-    def __init__(self,url:str,cantidad_paginas:int=10) -> None:
+    url =  'https://crypto.com/price' #url principal
+    def __init__(self,cantidad_paginas:int=10) -> None:
         self.driver = webdriver.Chrome()
-        self.url = url
         self.cantidad_paginas=cantidad_paginas
         self.pagina = 1
         self.__html_raw = " " #tags a partir de la tabla
         self.raw_data = [] #lista de tablas
+        self.continuar= False
+        self.current_url = '' #cada pagina en la que va pasando
 
 
     def call(self)->None:
-        self.driver.get(self.url) #vamos a la pagina
-        time.sleep(10)
-        button = self.driver.find_element(By.ID, "onetrust-reject-all-handler")                       
-        if button.is_displayed(): # Comprueba si el botón está visible
-            button.click()
+        if self.pagina !=1:
+            self.driver.get(self.current_url)
+        else:
+            self.driver.get(self.url) #vamos a la pagina
+            time.sleep(10)
+            button = self.driver.find_element(By.ID, "onetrust-reject-all-handler")                       
+            if button.is_displayed(): # Comprueba si el botón está visible
+             button.click()
 
 
     def busqueda(self)->None:
@@ -40,7 +45,7 @@ class Scrapp:
         trs = soup.find_all("tr", class_="css-1cxc880") #estos son las filas de la tabla
         for tr in trs: 
             row_data ={}
-            row_data['fecha'] = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+            row_data['fecha'] = datetime.now().strftime("%Y-%m-%d/%H:%M:%S")
             td = tr.find("td", class_="css-1sem0fc")
             row_data['nombre'] =td.find('p').text #primer valor
             row_data['siglas'] = td.find('span').text #segundo valor
@@ -54,7 +59,7 @@ class Scrapp:
             self.raw_data.append(row_data) #almaceno en lista
 
     def transform(self)->None:
-        fecha = datetime.now().strftime("%Y-%m-%d%H:%M:%S")
+        fecha = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
         archivo= f"./data/datos_{fecha}.csv"
         df = pd.DataFrame(self.raw_data)
         df.to_csv(archivo,index=False)
@@ -64,25 +69,43 @@ class Scrapp:
         if self.pagina < self.cantidad_paginas:
             self.pagina+=1
             add_str=f'?page{self.pagina}'
-            pagina = self.url +add_str
-            element = self.driver.find_element(By.CSS_SELECTOR,'button.chakra-button.css-1c62rym')
+            self.current_url = self.url +add_str
+            elements = self.driver.find_elements(By.CSS_SELECTOR,'button.chakra-button.css-1c62rym')
+            
+            # Find the specific element whose text matches the current page number
+            element = next((el for el in elements if el.text == str(self.pagina)), None)
+
             actions = ActionChains(self.driver)
             actions.move_to_element(element).perform()
-            if element.text=='2':
-                element.click()
+            element.click()
             time.sleep(5)
-            print(pagina)
+        else:
+            print('finalizado')
+            self.driver.quit()
+
+            
+
+        return self.current_url
+    
+    
+        
             
 def test():
     start_time = time.perf_counter()
-    prueba = Scrapp('https://crypto.com/price',10)
-    prueba.call()
-    prueba.busqueda()
-    prueba.get_data()
-    prueba.transform()
-    end_time = time.perf_counter()
+    
+    prueba = Scrapp(10)
+    while prueba.continuar:
+        prueba.call()
+        prueba.busqueda()
+        prueba.get_data()
+        prueba.transform()
+        prueba.next()
+        prueba.continuar = prueba.next()
+
+    end_time = time.perf_counter()    
     execution_time = end_time - start_time
-    prueba.next()
+    
+    
     print(f"Execution time: {execution_time:.4f} seconds")
             
     
